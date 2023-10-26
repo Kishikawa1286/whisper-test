b714a6be26234b853dd7c6cc0d5f281fbb133402
Kishikawa1286 <sanbasan.dev@gmail.com>
2023-10-26 17:18:39 +0000

    whisper と Moseca 直列つなぎ

--- src/services/transcribe.py
+++ src/services/transcribe.py
@@ -7,16 +7,21 @@ import numpy as np
 from utils.resample import resample as resample_audio
 
 
-def transcribe(model: whisper.Whisper, audiofile: str) -> Dict[str, Any]:
-    # Read the audio file into a numpy array
-    audio_data_np, sample_rate = sf.read(audiofile)
+def transcribe(
+        model: whisper.Whisper,
+        wave: np.ndarray,
+        sample_rate: int) -> Dict[str, Any]:
+    if len(wave.shape) != 1 and len(wave.shape) != 2:
+        raise ValueError(
+            "Audio data must be mono or stereo, not {}".format(
+                wave.shape))
 
     # Convert to monaural if the audio has multiple channels
-    if len(audio_data_np.shape) == 2 and audio_data_np.shape[1] >= 2:
-        audio_data_np = np.mean(audio_data_np, axis=1)
+    if len(wave.shape) == 2 and wave.shape[1] >= 2:
+        wave = np.mean(wave, axis=1)
 
     # Resample the audio data if necessary
-    audio_data_tensor = resample_audio(audio_data_np, sample_rate)
+    audio_data_tensor = resample_audio(wave, sample_rate)
 
     result = model.transcribe(audio_data_tensor)
 
